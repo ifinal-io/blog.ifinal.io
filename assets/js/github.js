b71d1e63949ccf7d7ba6dbf58c6286d353d344c9
likly <likly@ilikly.com>
2021-05-10 18:20:31 +0800

    github oauth

--- assets/js/github.js
+++ assets/js/github.js
@@ -11,7 +11,10 @@ Github = {
       url: 'https://github.com/login/oauth/access_token',
       type: 'POST',
       headers: {
-
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        'origin': 'https://blog.ifinal.io',
+        'referer': 'https://blog.ifinal.io/',
       },
       data: params,
       success: success,
